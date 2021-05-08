f4b729ecd52acf8ea6d37a4990a0ec2a71fc7692
alicek106 <alice_k106@naver.com>
2021-05-08 17:28:53 +0900

    Add pkg directory

---
+++ pkg/aws/ec2.go
@@ -0,0 +1,303 @@
+package aws
+
+import (
+	"fmt"
+	"github.com/alicek106/go-ec2-ssh-autoconnect/pkg/config"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"log"
+	"os"
+	"sort"
+	"strings"
+	"text/tabwriter"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+var svc *AwsEc2Manager
+
+func GetEC2Service() *AwsEc2Manager{
+	if svc == nil {
+		svc = &AwsEc2Manager{Ec2StartWaitTimeout: 30}
+		svc.CheckCredentials()
+	}
+	return svc
+}
+
+// Ec2InstanceInfo : Data struct for storing EC2 instance data
+type Ec2InstanceInfo struct {
+	instancdID   string
+	instanceName string
+	status       string
+	publicIP     string
+}
+
+// AwsEc2Manager : AWS EC2 Session and client amanger
+type AwsEc2Manager struct {
+	session             *session.Session
+	client              *ec2.EC2
+	Ec2StartWaitTimeout int
+}
+
+// CheckCredentials : Check existing credential from shell or configuartion
+func (aem *AwsEc2Manager) CheckCredentials() {
+	// TODO : Check Credential from configuration or env var
+	accessID := os.Getenv("AWS_ACCESS_KEY_ID")
+	secretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+	var err error
+	if len(accessID) == 0 || len(secretKey) == 0 {
+		log.Printf("Cannot find credential in environment variable.")
+		ep := config.GetEnvparser()
+		accessID, secretKey, err = ep.GetCredentials()
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			log.Println("Found credential in configuration file.")
+		}
+	} else {
+		log.Println("Found credential variable in environment variables")
+	}
+
+	aem.ValidateCredential(accessID, secretKey)
+}
+
+// ValidateCredential : Validate AWS Credential
+func (aem *AwsEc2Manager) ValidateCredential(accessID string, secretKey string) {
+	sess, err := session.NewSession()
+	switch err.(type) {
+	default: // no error
+		session.Must(sess, err)
+	case session.SharedConfigAssumeRoleError:
+		log.Fatal("Error from shared aws config credential assume role (SharedConfigAssumeRoleError). Abort")
+	case error:
+		log.Fatal(err.Error())
+	}
+
+	// Load session
+	aem.session = session.Must(session.NewSession(&aws.Config{
+		Region:      aws.String("ap-northeast-2"),
+		Credentials: credentials.NewStaticCredentials(accessID, secretKey, ""),
+	}))
+
+	// Test AWS function using provided credential
+	aem.client = ec2.New(aem.session)
+	_, err = aem.client.DescribeInstances(nil)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("Succeed to validate AWS credential.")
+	}
+}
+
+// GetInstanceIDs : Return instance IDs from instanceNames
+func (aem *AwsEc2Manager) GetInstanceIDs(instanceNames []string) []*string {
+	var instanceIDs = []*string{}
+	for _, instanceName := range instanceNames {
+		// If filters is defined directly in input parameter, it triggers syntax error.
+		filter := aem.getFilterForName(instanceName)
+		result, err := aem.client.DescribeInstances(filter)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			if len(result.Reservations) == 0{
+				log.Fatalf("Cannot find instance name [%s]. Abort", instanceName)
+			}
+			instanceIDs = append(instanceIDs, result.Reservations[0].Instances[0].InstanceId)
+		}
+	}
+	return instanceIDs
+}
+
+// ListInstances : List multiple instances.
+func (aem *AwsEc2Manager) ListInstances() {
+	result, err := aem.client.DescribeInstances(nil)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		// var ec2NameMaxLength int
+		var ec2InstanceList = []Ec2InstanceInfo{}
+		for _, reservation := range result.Reservations {
+			for _, instance := range reservation.Instances {
+				var ec2InstanceInfo = Ec2InstanceInfo{
+					status:     *instance.State.Name,
+					instancdID: *instance.InstanceId,
+				}
+
+				if instance.PublicIpAddress == nil {
+					ec2InstanceInfo.publicIP = "Unknown"
+				} else {
+					ec2InstanceInfo.publicIP = *instance.PublicIpAddress
+				}
+
+				for _, tag := range instance.Tags {
+					if *tag.Key == "Name" {
+						ec2InstanceInfo.instanceName = *tag.Value
+					}
+				}
+				ec2InstanceList = append(ec2InstanceList, ec2InstanceInfo)
+			}
+		}
+
+		sort.Slice(ec2InstanceList, func(i, j int) bool {
+			return ec2InstanceList[i].instanceName < ec2InstanceList[j].instanceName
+		})
+
+		writer := tabwriter.NewWriter(os.Stdout, 16, 8, 2, '\t', 0)
+		fmt.Fprintln(writer, "Instance ID\tInstance Name\tIP Address\tStatus")
+		for _, ec2InstanceInfo := range ec2InstanceList {
+			formatting := fmt.Sprintf("%s\t%s\t%s\t%s", ec2InstanceInfo.instancdID,
+				ec2InstanceInfo.instanceName, ec2InstanceInfo.publicIP, ec2InstanceInfo.status)
+			fmt.Fprintln(writer, formatting)
+		}
+
+		writer.Flush()
+	}
+}
+
+// StartInstances : Start multiple instances.
+func (aem *AwsEc2Manager) StartInstances(instanceIDs []*string) {
+	input := &ec2.StartInstancesInput{
+		InstanceIds: instanceIDs, // It should be used with pointer
+		DryRun:      aws.Bool(true),
+	}
+	_, err := aem.client.StartInstances(input)
+	awsErr, ok := err.(awserr.Error)
+
+	if ok && awsErr.Code() == "DryRunOperation" {
+		input.DryRun = aws.Bool(false)
+		_, err := aem.client.StartInstances(input)
+		if err != nil {
+			log.Fatal("Error", err)
+		} else {
+			log.Printf("Succeed to start EC2 instances.")
+		}
+	} else { // This could be due to a lack of permissions
+		log.Fatal("Error", err)
+	}
+}
+
+// WaitUntilActive : Wait unil all instances are up and running.
+// Order of instanceIDs and instanceNames is same. (Refer to GetInstanceIDs)
+func (aem *AwsEc2Manager) WaitUntilActive(instanceIDs []*string, instanceNames []string) {
+	for index := range instanceIDs {
+		log.Printf("Start to waiting EC2 instance %s (instance ID : %s)...", instanceNames[index], *instanceIDs[index])
+		for tries := 1; tries <= aem.Ec2StartWaitTimeout; tries++ {
+			// If EC2 instance is not running state
+			if aem.GetInstanceStatus(instanceIDs[index:index+1]) != "running" {
+				// If EC2 instance is not running state after 30s, continue to next instance.
+				if tries == 30 {
+					log.Printf("Failed to wait for EC2 instance to be active.")
+					break
+				} else {
+					// Wait seconds (aem.Ec2StartWaitTimeout)
+					log.Printf("Waiting for starting EC2 instance.. %d tries.", tries)
+					time.Sleep(time.Second)
+				}
+			} else {
+				// EC2 instance is already running, pass to wait 30 seconds for warm-up
+				if tries == 1 {
+					log.Printf("EC2 instance is in active.")
+					break
+				}
+				// Wait 30 seconds until SSH daemon is ready
+				log.Printf("EC2 instance is in active. Waiting for 30 seconds for warm-up.")
+				time.Sleep(30 * time.Second)
+				break
+			}
+		}
+	}
+}
+
+// GetInstancePublicIP : Return EC2 instance public IP from instance name
+func (aem *AwsEc2Manager) GetInstancePublicIP(instanceName string) (publicIP string) {
+	filter := aem.getFilterForName(instanceName)
+	result, err := aem.client.DescribeInstances(filter)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return *result.Reservations[0].Instances[0].PublicIpAddress
+}
+
+// StopInstances : Stop multiple instances.
+func (aem *AwsEc2Manager) StopInstances(instanceIDs []*string) {
+	input := &ec2.StopInstancesInput{
+		InstanceIds: instanceIDs, // It should be used with pointer
+		DryRun:      aws.Bool(true),
+	}
+	_, err := aem.client.StopInstances(input)
+	awsErr, ok := err.(awserr.Error)
+
+	if ok && awsErr.Code() == "DryRunOperation" {
+		input.DryRun = aws.Bool(false)
+		_, err := aem.client.StopInstances(input)
+		if err != nil {
+			log.Fatal("Error", err)
+		} else {
+			log.Printf("Succeed to stop EC2 instances.")
+		}
+	} else { // This could be due to a lack of permissions
+		log.Fatal("Error", err)
+	}
+}
+
+// GetUsernamePerOS : Return proper SSH username per EC2 instsance OS image
+func (aem *AwsEc2Manager) GetUsernamePerOS(instanceName string) (sshUsername string){
+	filter := aem.getFilterForName(instanceName)
+	result, err := aem.client.DescribeInstances(filter)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	amiImageID := *result.Reservations[0].Instances[0].ImageId
+	image, err := aem.client.DescribeImages(&ec2.DescribeImagesInput{
+		ImageIds: []*string{
+			aws.String(amiImageID),
+		},
+	})
+	if err != nil{
+		fmt.Println(err)
+	}
+
+	imageName := *image.Images[0].Name
+	if strings.Contains(imageName, "ubuntu") {
+		log.Printf("Found AMI name : %s, trying to ssh using ubuntu", imageName)
+		return "ubuntu"
+	}else if(strings.Contains(imageName, "amazon") || strings.Contains(imageName, "redhat")){
+		log.Printf("Found AMI name : %s, trying to ssh using ec2-user", imageName)
+		return "ec2-user"
+	}
+
+	log.Fatalf("Not found appropriate user name for ami : %s", imageName)
+	return ""
+}
+
+func (aem *AwsEc2Manager) GetInstanceStatus(instanceID []*string) (status string) {
+	input := &ec2.DescribeInstancesInput{
+		InstanceIds: instanceID,
+	}
+	result, err := aem.client.DescribeInstances(input)
+	if err != nil {
+		log.Println("Error in waiting for EC2 instances to be active")
+		log.Fatal(err)
+	} else {
+		status = *result.Reservations[0].Instances[0].State.Name
+	}
+	return status
+}
+
+// getFilterForName : Return filter for describing instances
+func (aem *AwsEc2Manager) getFilterForName(instanceName string) (input *ec2.DescribeInstancesInput) {
+	filters := []*ec2.Filter{
+		&ec2.Filter{
+			Name:   aws.String("tag:Name"),
+			Values: []*string{aws.String(instanceName)},
+		},
+	}
+	input = &ec2.DescribeInstancesInput{
+		Filters: filters,
+	}
+	return input
+}
---
+++ pkg/cmd/cmd.go
@@ -0,0 +1,220 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"github.com/alicek106/go-ec2-ssh-autoconnect/pkg/aws"
+	"github.com/alicek106/go-ec2-ssh-autoconnect/pkg/config"
+	"github.com/spf13/cobra"
+	"log"
+	"os"
+	"os/exec"
+)
+
+var (
+	key string
+	version      = "99.99.99"
+	buildDate    = "1970-01-01T00:00:00Z"
+	gitCommit    = ""
+)
+
+func NewListCommand() *cobra.Command {
+	versionCmd := cobra.Command{
+		Use:   "list",
+		Short: "List EC2 instances",
+		Example: `  # List all EC2 instances.
+  ec2-connect list`,
+		Run: func(cmd *cobra.Command, args []string) {
+			svc := aws.GetEC2Service()
+			svc.ListInstances()
+		},
+	}
+	return &versionCmd
+}
+
+func NewStartCommand() *cobra.Command {
+	startCmd := cobra.Command{
+		Use:   "start",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("Require a instance name.")
+			}
+			return nil
+		},
+		Short: "Start EC2 instances",
+		Example: `  # Start a EC2 instances.
+  ec2-connect start myserver`,
+		Run: func(cmd *cobra.Command, args []string) {
+			svc := aws.GetEC2Service()
+			instanceName := args[0]
+			instanceIDs := svc.GetInstanceIDs([]string{instanceName})
+			for index, instanceName := range []string{instanceName} {
+				log.Printf("Starting EC2 instance : %s (instance ID: %s)", instanceName, *instanceIDs[index])
+			}
+			svc.StartInstances(instanceIDs)
+		},
+	}
+	return &startCmd
+}
+
+func NewStopCommand() *cobra.Command {
+	stopCmd := cobra.Command{
+		Use:   "stop",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("Require a instance name.")
+			}
+			return nil
+		},
+		Short: "Stop EC2 instances",
+		Example: `  # Stop a EC2 instances.
+  ec2-connect stop myserver`,
+		Run: func(cmd *cobra.Command, args []string) {
+			svc := aws.GetEC2Service()
+			instanceIDs := svc.GetInstanceIDs([]string{args[0]})
+			for index, instanceName := range []string{args[0]} {
+				log.Printf("Stoping EC2 instance : %s (instance ID: %s)", instanceName, *instanceIDs[index])
+			}
+			svc.StopInstances(instanceIDs)
+		},
+	}
+	return &stopCmd
+}
+
+func NewGroupCommand() *cobra.Command {
+	groupCmd := cobra.Command{
+		Use:   "group",
+		Short: "Manage EC2 instance as a group",
+		Example: `  # Start a group of instances
+  ec2-connect group start mygroup
+
+  # Stop EC2 instance as a group
+  ec2-connect group stop mygroup`,
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.HelpFunc()(cmd, args)
+			os.Exit(1)
+		},
+	}
+
+	groupCmd.AddCommand(newGroupStartCommand())
+	groupCmd.AddCommand(newGroupStopCommand())
+	return &groupCmd
+}
+
+func newGroupStartCommand() *cobra.Command {
+	groupStartCmd := cobra.Command{
+		Use:   "start",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("Require a group name.")
+			}
+			return nil
+		},
+		Short: "Start EC2 instance as a group",
+		Example: `  # Start a group of instances
+  ec2-connect group start mygroup`,
+		Run: func(cmd *cobra.Command, args []string) {
+			svc := aws.GetEC2Service()
+			instanceNames := config.GetEnvparser().GetGroupInstanceNames(args[0])
+			instanceIDs := svc.GetInstanceIDs(instanceNames)
+			for index, instanceName := range instanceNames {
+				log.Printf("Starting EC2 instance : %s (instance ID: %s)", instanceName, *instanceIDs[index])
+			}
+			svc.StartInstances(instanceIDs)
+		},
+	}
+	return &groupStartCmd
+}
+
+func newGroupStopCommand() *cobra.Command {
+	groupStopCmd := cobra.Command{
+		Use:   "stop",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("Require a group name.")
+			}
+			return nil
+		},
+		Short: "Stop EC2 instance as a group",
+		Example: `  # Stop a group of instances
+  ec2-connect group stop mygroup`,
+		Run: func(cmd *cobra.Command, args []string) {
+			svc := aws.GetEC2Service()
+			instanceNames := config.GetEnvparser().GetGroupInstanceNames(args[0])
+			instanceIDs := svc.GetInstanceIDs(instanceNames)
+			for index, instanceName := range instanceNames {
+				log.Printf("Stoping EC2 instance : %s (instance ID: %s)", instanceName, *instanceIDs[index])
+			}
+			svc.StopInstances(instanceIDs)
+		},
+	}
+	return &groupStopCmd
+}
+
+func NewConnectCommand() *cobra.Command {
+	connectCmd := cobra.Command{
+		Use:   "connect",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("Require a instance name.")
+			}
+			return nil
+		},
+		Short: "Connect to a EC2 instance using SSH",
+		Example: `  # Connect to a EC2 instance
+  ec2-connect connect myserver
+
+  # Connect to a EC2 instance using user-defined key in configuration file.
+  ec2-connect connect myserver --key=mykey`,
+		Run: func(cmd *cobra.Command, args []string) {
+			svc := aws.GetEC2Service()
+
+			if cmd.Flags().Changed("key") {
+				keyName, err := cmd.Flags().GetString("key")
+				key = config.GetEnvparser().GetCustomKey(keyName)
+				if err != nil {
+					log.Fatalf("Something wrong to get key flag: %s", err)
+				}
+			}
+			instanceName := args[0]
+			instanceID := svc.GetInstanceIDs([]string{instanceName})
+			if svc.GetInstanceStatus(instanceID) == "running" {
+				log.Println("Instance in active.")
+			} else {
+				svc.StartInstances(instanceID)
+				svc.WaitUntilActive(instanceID, []string{instanceName})
+			}
+
+			instanceIP := svc.GetInstancePublicIP(instanceName)
+			sshUserName := svc.GetUsernamePerOS(instanceName)
+			execCmd := exec.Command("ssh", "-oStrictHostKeyChecking=no", fmt.Sprintf("%s@%s", sshUserName, instanceIP), fmt.Sprintf("-i%s", key))
+			execCmd.Stdin = os.Stdin
+			execCmd.Stdout = os.Stdout
+			execCmd.Stderr = os.Stderr
+			err := execCmd.Run()
+			if err != nil {
+				log.Fatalf("Error: %s", err)
+			}
+		},
+	}
+
+	defaultKey := config.GetEnvparser().GetDefaultKey()
+	connectCmd.Flags().StringVar(&key, "key", defaultKey, "SSH key to connect EC2 instance")
+	return &connectCmd
+}
+
+func NewVersionCommand() *cobra.Command {
+	versionCmd := cobra.Command{
+		Use:   "version",
+		Short: "Print version information",
+		Example: `  # Print version of binary.
+  ec2-connect version
+`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("  Version: %s\n", version)
+			fmt.Printf("  Build Date: %s\n", buildDate)
+			fmt.Printf("  Git Commit: %s\n", gitCommit)
+		},
+	}
+	return &versionCmd
+}
